c393eeb82fe6006f31c176f81bc71c5e13bb180b
HenriqueBros <58406947+HenriqueBros@users.noreply.github.com>
2020-08-29 03:46:43 -0300

    Cap03

---
+++ CAP03/Array/Exemplo01.js
@@ -0,0 +1,9 @@
+let daysOfWeek = [];
+
+daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday', 'Saturday'];
+
+console.log(daysOfWeek.length);
+
+for(let i = 0; i < daysOfWeek.length; i++ ) {
+    console.log(daysOfWeek[i]);
+}
