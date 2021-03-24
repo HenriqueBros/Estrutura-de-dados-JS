b9697e39dcccf26a28614b4150d7230898d4e49e
HenriqueBros <hmbroseghini@hotmail.com>
2021-03-24 11:27:06 -0300

    rmove_test

---
+++ CAPITULOS/CAP10/BinarySearchTree.js
@@ -0,0 +1,50 @@
+import { Compare, defaultCompare} from './util';
+import { Node } from './node';
+
+export default class BinarySearchTree {
+    insert(key) {
+        if(this.root == null) {
+            this.root = new Node(key);
+        } else {
+            this.insertNode(this.root, key);
+        }
+    }
+
+    insertNode(node, key) {
+        if(this.compareFn(key, node.key) === Compare.LESS_THAN) {
+            if(node.left == null) {
+                node.left = new Node(key);
+            } else {
+                this.insertNode(node.left, key);
+            }
+        } else {
+            if(node.right == null) {
+                node.right = new Node(key);
+            } else {
+                this.insertNode(node.right, key);
+            }
+        }
+    }
+
+    constructor(compareFn = defaultCompare) {
+        this.compareFn = compareFn;
+        this.root = null;
+    }
+}
+
+const tree = new BinarySearchTree();
+tree.insert(11);
+tree.insert(7);
+tree.insert(15);
+tree.insert(5);
+tree.insert(3);
+tree.insert(9);
+tree.insert(8);
+tree.insert(10);
+tree.insert(13);
+tree.insert(12);
+tree.insert(14);
+tree.insert(20);
+tree.insert(18);
+tree.insert(25);
+tree.insert(6);
